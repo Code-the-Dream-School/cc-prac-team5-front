3120db92461a1801b138088a83fa8f87319dccf6
Armanchi <armanijohnson0104@gmail.com>
2023-07-06 19:32:44 -0500

    updated child dashboard

--- src/Components/ChildViews/Dashboard/components/ChildDashboard.js
+++ src/Components/ChildViews/Dashboard/components/ChildDashboard.js
@@ -1,35 +1,48 @@
-import React, { useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import '../styles/ChildDashboard.css';
 import background from '../../../../assets/background.jpg';
 import { Container, Box } from '@mui/system';
-// import axios from 'axios';
-
-const tasks = [
-  {
-      title: 'vacuum roof',
-      status: 'pending'
-  },
-  {
-      title: 'feed dog',
-      status: 'complete'
-  },
-  {
-      title: 'walk dog',
-      status: 'started'
-  },
-  {
-    title: 'clean room',
-    status: 'complete'
-}
-]
+import axios from 'axios';
+import { Typography } from '@mui/material';
 
 const ChildDashboard = () => {
-  const [taskList, setTaskList] = useState(tasks);
+  const [tasks, setTasks] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [username, setUsername] = useState('');
+  const [userImage, setUserImage] = useState('');
 
-  const handleStatusChange = (index, status) => {
-    const updatedTasks = [...taskList];
-    updatedTasks[index].status = status;
-    setTaskList(updatedTasks);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+      try {
+        const tasksResponse = await axios.get(
+          `${process.env.REACT_APP_SERVICE_ENDPOINT}/tasks`
+        );
+        if (tasksResponse.data.tasks) {
+          setTasks(tasksResponse.data.tasks);
+        }
+
+        const userResponse = await axios.get(
+          `${process.env.REACT_APP_SERVICE_ENDPOINT}/children/:id`
+        );
+        setUsername(userResponse.data.userName);
+        setUserImage(userResponse.data.userImage);
+
+        setIsLoading(false);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+ 
+
+  const handleStatusChange = (taskId, status) => {
+    // Update the status of the task in the backend 
+    console.log(`Task ID: ${taskId}, Status: ${status}`);
   };
 
   return (
@@ -38,109 +51,42 @@ const ChildDashboard = () => {
         <img src={background} alt='backgr' className='Background' />
       </div>
       <Container>
-        <div>
+        <Box sx={{display:'flex', flexDirection:'column', justifyContent:'center'}}> 
+          <Typography sx={{textAlign:'center',pt: 15, fontSize:25, fontWeight:'bold'}}>Hi, {username}!</Typography>
+          <img src={userImage} alt='user' className='UserImage' />
+        </Box> 
+        <Box sx={{ display: 'flex', justifyContent: 'center', width:'100%', pt:10 }}>
           <div>
-            <Box sx={{display:'flex', justifyContent:'center', pt: 5, pb:'30%', fontSize:25, fontWeight: 700}}>Hi, User1</Box>
+            {isLoading ? (
+              <p>Loading tasks...</p>
+            ) : (
+              <table className='taskTable'>
+                <tbody className='taskList'>
+                  {tasks.map((task) => (
+                    <tr key={task.id}>
+                      <td>{task.title}</td>
+                      <td>
+                        <select
+                          value={task.status}
+                          onChange={(e) =>
+                            handleStatusChange(task.id, e.target.value)
+                          }
+                        >
+                          <option value='pending'>Pending</option>
+                          <option value='started'>Started</option>
+                          <option value='complete'>Complete</option>
+                        </select>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            )}
           </div>
-          <Box sx={{display: 'flex', justifyContent:'center', flexDirection:'column', pt: 5}}>
-            <table className='taskTable'>
-            <Box sx={{fontSize:22,textAlign:'center', alignSelf:'center'}}>User1's Chores</Box>
-              <tbody className='taskList'>
-                {taskList.map((task, index) => (
-                  <tr key={index}>
-                    <td className='taskTitle'>{task.title}</td>
-                    <td>
-                      <select
-                        value={task.status}
-                        onChange={(e) =>
-                          handleStatusChange(index, e.target.value)
-                        }
-                      >
-                        <option value='pending'>Pending</option>
-                        <option value='started'>Started</option>
-                        <option value='complete'>Complete</option>
-                      </select>
-                    </td>
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </Box>
-        </div>
+        </Box>
       </Container>
     </div>
   );
 };
-
-
-// const ChildDashboard = () => {
-//   const [tasks, setTasks] = useState([]);
-//   const [isLoading, setIsLoading] = useState(false);
-
-//   useEffect(() => {
-//     const getTasks = async () => {
-//       setIsLoading(true);
-//       try {
-//         const response = await axios.get(
-//           `${process.env.REACT_APP_SERVICE_ENDPOINT}/tasks`
-//         );
-//         if (response.data.tasks) {
-//           setTasks(response.data.tasks);
-//           setIsLoading(false);
-//         }
-//       } catch (error) {
-//         console.log(error);
-//       }
-//     };
-//     getTasks();
-//   }, []);
-
-//   const handleStatusChange = (taskId, status) => {
-//     // Update the status of the task in the backend 
-//     console.log(`Task ID: ${taskId}, Status: ${status}`);
-//   };
-
-//   return (
-//     <div>
-//       <div>
-//         <img src={background} alt='backgr' className='Background' />
-//       </div>
-//       <Container>
-        //   <div>
-        //     <Box sx={{display:'flex', justifyContent:'center', pt: 5, fontSize:25, fontWeight: 700}}>Hi, User1</Box>
-        //   </div> 
-        //   <Box sx={{display: 'flex', justifyContent:'center', flexDirection:'column', pt: 5}}>
-        //  <div>
-        //    {isLoading ? (
-//             <p>Loading tasks...</p>
-//           ) : (
-//             <table className='taskTable'>
-//               <tbody className='taskList'>
-//                 {tasks.map((task) => (
-//                   <tr key={task.id}>
-//                     <td>{task.title}</td>
-//                     <td>
-//                       <select
-//                         value={task.status}
-//                         onChange={(e) =>
-//                           handleStatusChange(task.id, e.target.value)
-//                         }
-//                       >
-//                         <option value='pending'>Pending</option>
-//                         <option value='started'>Started</option>
-//                         <option value='complete'>Complete</option>
-//                       </select>
-//                     </td>
-//                   </tr>
-//                 ))}
-//               </tbody>
-//             </table>
-//           )}
-            // </Box>
-//         </div>
-//       </Container>
-//     </div>
-//   );
-// };
 
 export default ChildDashboard;
